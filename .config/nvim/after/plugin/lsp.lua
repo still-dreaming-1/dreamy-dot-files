4b4e8d8178d647a4e69afd659ca26c261d4363cc
still-dreaming-1 <jessedickey@gmail.com>
2023-02-03 20:35:02 +0000

    nvim: Fixed LSP regression issue
    stopped understanding nvim lua configs

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,5 +1,6 @@
 local lsp = require 'lsp-zero'
 lsp.preset('recommended')
+
 lsp.set_preferences({
     set_lsp_keymaps = {
         omit = {
@@ -7,10 +8,13 @@ lsp.set_preferences({
         }
     },
 })
+
 lsp.on_attach(function(client, bufnr)
     local opts = {buffer = bufnr, remap = false}
 
     -- Displays signature information about the symbol under the cursor in a floating window
     vim.keymap.set('n', '<leader><leader>k', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
 end)
+
+lsp.nvim_workspace() -- Configure lua language server for neovim
 lsp.setup()
