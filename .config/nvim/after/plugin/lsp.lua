f7c7ae6138f4b624f3e2fae03eb8251900b673b5
still-dreaming-1 <jessedickey@gmail.com>
2023-02-10 20:58:33 +0000

    nvim: removed LSP added key binding

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -1,18 +1,24 @@
 if not vim.g.vscode then
-    local lsp = require 'lsp-zero'
-    lsp.preset('recommended')
-
-    lsp.set_preferences({
+    local lsp = require('lsp-zero').preset({
+        name = 'minimal',
         set_lsp_keymaps = {
-            omit = {
-                '<C-k>',
-            }
+            omit = {'<C-k>'},
         },
+        manage_nvim_cmp = true,
+        suggest_lsp_servers = true,
+    })
+
+    lsp.setup_nvim_cmp({
+        mapping = lsp.defaults.cmp_mappings({
+            ['<CR>'] = vim.NIL,
+        })
     })
 
     lsp.on_attach(function(client, bufnr)
-        local opts = {buffer = bufnr, remap = false}
-
+        local opts = {
+            buffer = bufnr,
+            remap = false
+        }
         -- Displays signature information about the symbol under the cursor in a floating window
         vim.keymap.set('n', '<leader><leader>k', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
     end)
