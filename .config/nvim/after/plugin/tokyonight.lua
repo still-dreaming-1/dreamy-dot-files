746f7fba3160be15d8d95a8ce0e3bdf7335fae88
still-dreaming-1 <jessedickey@gmail.com>
2023-02-10 22:05:32 +0000

    nvim: improved colors:
    Set a specific foreground color for variables to use instead of the
    plain whitish one that was there before.

--- .config/nvim/after/plugin/tokyonight.lua
+++ .config/nvim/after/plugin/tokyonight.lua
@@ -1,4 +1,11 @@
 if not vim.g.vscode then
+    require("tokyonight").setup({
+        terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+        styles = {
+            -- Value is any valid attr-list value for `:help nvim_set_hl`
+            variables = { fg = '#00EBAC' },
+        },
+    })
     vim.cmd 'colorscheme tokyonight-moon'
 
     local dreamyIsMoon = true
